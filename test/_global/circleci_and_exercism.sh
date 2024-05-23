14ac6e5dae308515d6eca82dc127e16277a6c3e1
Cody Taylor <codemister99@yahoo.com>
2024-05-23 03:46:29 -0400

    Use pipe to force non-interactive circleci usage

--- test/_global/circleci_and_exercism.sh
+++ test/_global/circleci_and_exercism.sh
@@ -6,7 +6,7 @@ source dev-container-features-test-lib
 
 check "circleci location" bash -c "ls /usr/bin/circleci"
 check "exercism location" bash -c "ls /usr/bin/exercism"
-check "circleci info help" bash -c "circleci --skip-update-check info --help"
+check "circleci setup help" bash -c "circleci --skip-update-check setup --help | cat"
 check "exercism configure help" bash -c "exercism configure --help"
 
 reportResults
--- test/circleci-cli/test.sh
+++ test/circleci-cli/test.sh
@@ -5,6 +5,6 @@ set -e
 source dev-container-features-test-lib
 
 check "circleci location" bash -c "ls /usr/local/bin/circleci"
-check "version" bash -c "circleci --skip-update-check version"
+check "version" bash -c "circleci --skip-update-check version | cat"
 
 reportResults
